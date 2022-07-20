ef1081d3daeae06fbbc4f83c1ea472b5c687e44c
Mandeep Singh <mandeepsinghsaggu2@gmail.com>
2022-07-20 16:30:29 +0530

    L

--- app/src/main/java/com/mandeep/compose_app_/Screens/ScreenOne.kt
+++ app/src/main/java/com/mandeep/compose_app_/Screens/ScreenOne.kt
@@ -1,5 +1,10 @@
 package com.mandeep.compose_app_.Screens
 
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
@@ -8,13 +13,16 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavHostController
+
 
 @Composable
 fun ScreenOne(navHostController: NavHostController){
 
-    Column {
 
+
+    Column {
         Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.TopCenter){
             Text("ScreenOne", color = Color.White)
         }
@@ -28,6 +36,5 @@ fun ScreenOne(navHostController: NavHostController){
                 Text("Go to Screen 2")
             }
         }
-
     }
 }
